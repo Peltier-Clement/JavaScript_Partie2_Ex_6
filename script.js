7f1af0519ee691bc72c65ac28e7cf668a6965b03
nisted <clement.p10@laposte.net>
2018-10-09 11:05:52 +0200

    premier depot

---
+++ script.js
@@ -0,0 +1,14 @@
+function mouse(){
+  document.getElementById('image1').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image1.jpg";
+  document.getElementById('image2').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image2.jpg";
+  document.getElementById('image3').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image3.jpg";
+  document.getElementById('image4').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image4.jpg";
+  document.getElementById('image5').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image5.jpg";
+}
+function mouse1(){
+  document.getElementById('image1').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image1_2.jpg";
+  document.getElementById('image2').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image2_2.jpg";
+  document.getElementById('image3').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image3_2.jpg";
+  document.getElementById('image4').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image4_2.jpg";
+  document.getElementById('image5').src="/home/peltier/javascript/JavaScript_Partie2/Ex_6/assets/image5_2.jpg";
+}
